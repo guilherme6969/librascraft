a16802c113020011d6732f1274ed5f65956f9e81
guilherme6969 <shockguilhermecolombo@gmail.com>
2020-04-17 16:08:38 -0300

    la versao

---
+++ cozinha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, inicial-scale-to-fit-no">
+    <title>Librascraft</title>
+	<link href="css/bootstrap.min.css" rel="stylesssheet"/>
+	
+	<script src='js/jquery-3.4.1min.js'></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<style>
+	body {
+    background: url("img/cozinha.png") no-repeat center top fixed;
+
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+	</style>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ tentar.php
@@ -0,0 +1,5 @@
+<html>
+<head></head>
+<body background="img/fundo_principal.jpeg"
+</body>
+</html>
