3b7aa5e734c87c44e534df42314bcec3832ef3eb
guilherme6969 <shockguilhermecolombo@gmail.com>
2020-07-01 14:44:45 -0300

    Atualizacao layout

---
+++ sala2.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, inicial-scale=1, shrink-to-fit=no">
+    <title>Librascraft</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	
+	<script src='js/jquery-3.4.1.min.js'></script>
+	<script src="js/bootstrap.min.js"></script>
+	
+	<style>
+	body {
+    background: url("img/sala.png") no-repeat center top fixed;
+
+    -webkit-background-size: cover;
+    -moz-background-size: cover; 
+    -o-background-size: cover;
+    background-size: cover;
+}
+	</style>
+</head>
+<body>
+	<!-- A - div principal-->
+	<div class="container align-middle">
+		<!-- B (filha da principal - A)-->
+		<div class="row justify-content-center ">
+			<div class="row">
+				<div class="col d-flex flex-column justify-content-center align-items-center">
+					<!-- C (filha da div B) -->
+					<div class="col-12 border border-warning bg-white">
+						<!-- D (filha da div C) : LINHA -->
+			
+						<div class="row">
+							<div class="col py-3 px-md-3 border bg-light d-flex flex-column justify-content-center align-items-center">
+								<table border="1">
+									<tr>
+										<td><h1>S</h1></td>
+										<td><h1>O</h1></td>
+										<td><h1>F</h1></td>
+										<td><h1>Á</h1></td>
+									</tr>
+									<tr>
+										<td><img src="img/s.jpeg" alt="..."></td>
+										<td><img src="img/o.jpeg" alt="..."></td>
+										<td><img src="img/f.jpeg" alt="..."></td>
+										<td><img src="img/a.jpeg" alt="..."></td>
+									</tr>
+								</table>
+							</div>
+						</div>
+						<div class="row mt-4 mx-md-n5">
+							<div class="col py-3 px-md-3 border bg-light d-flex flex-column justify-content-center align-items-center">
+								<img src="img/image_sofa.jpeg" class="rounded float-left">
+							</div>
+							<div class="col py-3 px-md-3 border bg-light">
+								<iframe width="400" height="200" src="https://www.youtube.com/embed/Xxiry0x6jxE" class="rounded float-right" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
