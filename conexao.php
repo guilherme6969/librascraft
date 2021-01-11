62a8877b95cdc78f2110293f21c42e6831bb62fe
annajucunha <jukita_julia@hotmail.com>
2021-01-11 19:17:57 -0200

    Atualizaçao

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
 
     //local no qual o banco de dados está instalado
-    $local = "db4free.net:3306";
-    $usuario = "librascraft";
-    $senha = "lib15900";
-    $bd = "librascraft";
+    $local = "uranus.ignitionserver.net:3306";
+    $usuario = "bsts_librascraft";
+    $senha = "lib15900!";
+    $bd = "bsts_librascraft";
 
     $conexao = mysqli_connect($local,$usuario,$senha,$bd) 
                     or die("ERRO");
